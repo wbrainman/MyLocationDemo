7e7a53a99a3645b6a2e9fe05d8738318b3af4edf
wangbo <wbrainman@163.com>
2023-02-07 22:36:43 +0800

    2nd commit,can work

--- app/src/main/java/com/example/mylocationdemo/MainActivity.java
+++ app/src/main/java/com/example/mylocationdemo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.mylocationdemo;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
@@ -10,6 +11,7 @@ import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -26,16 +28,53 @@ public class MainActivity extends AppCompatActivity {
         mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onStart() {
         super.onStart();
         Log.d(TAG, "onStart: ");
+        checkPermissions();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void checkPermissions() {
+        Log.d(TAG, "checkPermissions: ");
+        boolean b1 = false;
+        boolean b2 = false;
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "checkPermissions: 111");
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1000);
+        } else {
+            b1 = true;
+        }
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "checkPermissions: 222");
+            requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1001);
+        } else {
+            b2 = true;
+        }
+
+        if (b1 && b2) {
+            onPermissionGranted();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        Log.d(TAG, "onRequestPermissionsResult: code " + requestCode
+                + " permissions " + permissions + " grantResults " + grantResults);
+        onPermissionGranted();
+    }
+
+    private void onPermissionGranted() {
         String bestProvider = mLocationManager.getBestProvider(getCriteria(), true);
         Log.d(TAG, "bestProvider " + bestProvider);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                        this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
             // here to request the missing permissions, and then overriding
@@ -47,7 +86,6 @@ public class MainActivity extends AppCompatActivity {
         }
         Location location = mLocationManager.getLastKnownLocation(bestProvider);
         Log.d(TAG, "onStart: getLastKnownLocation " + location);
-
         mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, locationListener);
     }
 
